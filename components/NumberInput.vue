fd08700f730422b603dda8f7f16ae72145b0a780
taharafi <taharafi8@gmail.com>
2020-08-29 11:46:55 +0200

    first commit

---
+++ components/NumberInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="main">
+    <h1>Application Test</h1>
+      <div v-for="(value,index) in listOfNumbers" v-bind:key="index">
+      <input type="Number"  v-model="value.number"  @change="onChange()"  ><br>
+      </div>
+      <button @click="addInput()">{{sum}}</button><br>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberInput',
+
+  data(){
+    return {
+        listOfNumbers:  [
+          {
+            number: ''
+          }
+        ] ,
+        sum: 0 ,
+    }
+  },
+  
+  methods : {
+
+    addInput(){
+      this.listOfNumbers.push({
+        number: ''
+      } 
+      ); 
+    },
+
+     onChange() {
+        this.sum=0;
+        for (var i = 0; i < this.listOfNumbers.length; i++) {
+        this.sum = this.sum + Number(this.listOfNumbers[i].number);
+       }     
+       }
+
+  }
+}
+</script>
+
+
+<style scoped>
+#main {
+  margin: 20px auto;
+  max-width: 700px;
+}
+input {
+  font-size: 30px;
+  border: 1px double rgb(102, 97, 96) ;
+  border-radius: 4px;
+   text-align: center;
+}
+button {
+ font-size: 30px;
+  border: 1px double rgb(102, 97, 96) ;
+  border-radius: 4px;
+  width: 380px;
+}
+</style>
